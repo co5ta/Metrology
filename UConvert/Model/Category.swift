c30f95b05f2eabb4d51a63060061dae9f6b1cabf
co5ta <costamonzili@gmail.com>
2020-10-11 17:11:20 +0200

    Add new categories

--- UConvert/Model/Category.swift
+++ UConvert/Model/Category.swift
@@ -12,15 +12,125 @@ import Foundation
 enum Category: String, CaseIterable {
     
     // Cases
-    case Length
-    case Mass
+    case Length, Area, Volume, Angle
+    case Mass, Pressure
+    case Acceleration, Duration, Speed
+    case Energy, Power, Temperature
+    case ElectricCharge
+    case FuelEfficiency
     
     /// Units available in a category
     var units: [Unit] {
         switch self {
         // Base units must take the first place at each array
-        case .Length: return [UnitLength.meters, UnitLength.miles, UnitLength.feet, UnitLength.inches]
-        case .Mass: return [UnitMass.kilograms, UnitMass.pounds, UnitMass.shortTons, UnitMass.ounces]
+        case .Length: return [
+                UnitLength.meters,
+                UnitLength.feet,
+                UnitLength.inches,
+                UnitLength.yards,
+                UnitLength.miles,
+                UnitLength.scandinavianMiles,
+                UnitLength.lightyears,
+                UnitLength.fathoms,
+                UnitLength.furlongs,
+                UnitLength.astronomicalUnits,
+                UnitLength.parsecs
+            ]
+        case .Area: return [
+                UnitArea.squareMeters,
+                UnitArea.squareInches,
+                UnitArea.squareFeet,
+                UnitArea.squareYards,
+                UnitArea.squareMiles,
+                UnitArea.hectares,
+                UnitArea.acres,
+                UnitArea.ares
+            ]
+        case .Volume: return [
+                UnitVolume.liters,
+                UnitVolume.cubicFeet,
+                UnitVolume.cubicYards,
+                UnitVolume.cubicMiles,
+                UnitVolume.acreFeet,
+                UnitVolume.bushels,
+                UnitVolume.teaspoons,
+                UnitVolume.fluidOunces,
+                UnitVolume.cups,
+                UnitVolume.metricCups,
+                UnitVolume.pints,
+                UnitVolume.quarts,
+                UnitVolume.gallons,
+                UnitVolume.imperialTeaspoons,
+                UnitVolume.imperialTablespoons,
+                UnitVolume.imperialPints,
+                UnitVolume.imperialQuarts,
+                UnitVolume.imperialGallons,
+                UnitVolume.imperialFluidOunces,
+            ]
+        case .Angle: return [
+                UnitAngle.degrees,
+                UnitAngle.radians,
+                UnitAngle.gradians,
+                UnitAngle.revolutions,
+                UnitAngle.arcMinutes,
+                UnitAngle.arcSeconds
+            ]
+        case .Mass: return [
+                UnitMass.kilograms,
+                UnitMass.pounds,
+                UnitMass.metricTons,
+                UnitMass.shortTons,
+                UnitMass.stones,
+                UnitMass.carats,
+                UnitMass.ounces,
+                UnitMass.ouncesTroy,
+                UnitMass.slugs
+            ]
+        case .Pressure: return [
+                UnitPressure.newtonsPerMetersSquared,
+                UnitPressure.bars,
+                UnitPressure.millimetersOfMercury,
+                UnitPressure.inchesOfMercury,
+                UnitPressure.poundsForcePerSquareInch
+            ]
+        case .Acceleration: return [
+                UnitAcceleration.metersPerSecondSquared,
+                UnitAcceleration.gravity
+            ]
+        case .Duration: return [
+                UnitDuration.seconds,
+                UnitDuration.minutes,
+                UnitDuration.hours
+            ]
+        case .Speed: return [
+                UnitSpeed.metersPerSecond,
+                UnitSpeed.kilometersPerHour,
+                UnitSpeed.milesPerHour,
+                UnitSpeed.knots
+            ]
+        case .Energy: return [
+                UnitEnergy.joules,
+                UnitEnergy.calories,
+                UnitEnergy.kilowattHours
+            ]
+        case .Power: return [
+                UnitPower.watts,
+                UnitPower.horsepower
+            ]
+        case .Temperature: return [
+                UnitTemperature.kelvin,
+                UnitTemperature.celsius,
+                UnitTemperature.fahrenheit
+            ]
+        case .ElectricCharge: return [
+                UnitElectricCharge.coulombs,
+                UnitElectricCharge.ampereHours
+            ]
+        case .FuelEfficiency: return [
+            UnitFuelEfficiency.litersPer100Kilometers,
+            UnitFuelEfficiency.milesPerGallon,
+            UnitFuelEfficiency.milesPerImperialGallon,
+            ]
         }
     }
 }
